98fac60e1d0c6b272c4510ffc22d0b4100b3b570
amit <amit@vimond.com>
2019-05-08 11:09:34 +1000

    New files

---
+++ first_s3/variable.tf
@@ -0,0 +1,17 @@
+variable "s3_bucket_name" {
+  default     = "amit-main-test"
+  description = "Name of the S3 bucket"
+  type        = "string"
+}
+
+variable "s3_region" {
+  type    = "list"
+  default = ["ca-central-1"]
+}
+
+locals {
+  s3_tags = {
+    created_by  = "terraform"
+    environment = "test"
+  }
+}
